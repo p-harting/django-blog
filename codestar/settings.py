114c8bd6cc1ef27f7f5d574ff1211285a84fbeb9
Pascal Harting <pascalharting98@gmail.com>
2024-08-10 04:39:35 +0000

    Prepare files for deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0o620$*hlf2oxj04%84dr&q64uo*%&^t4#^wnxfbztgn)jk^ns'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-blog-project-ci-6ea285b901f3.herokuapp.com',
+                 '.herokuapp.com',]
 
 
 # Application definition
